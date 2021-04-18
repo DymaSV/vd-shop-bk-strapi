520a57196b7af78cd245657b5feedf45957ed24e
DymaSv <dymasvyrydonov@gmail.com>
2021-04-17 22:38:57 -0400

    Get only needed lang from collection

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -4,20 +4,25 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-
-const { sanitizeEntity } = require('strapi-utils');
+// const { sanitizeEntity } = require('strapi-utils');
+const _ = require('lodash');
+const lang_env = ['fr', 'en'];
 
 module.exports = {
-    // async find(ctx) {
-    //     console.log(strapi.models.category)
+    async find(ctx) {
+        let lang = ctx.params.lang;
+        if(!lang_env.includes(lang)){
+            lang = 'en'
+        }
+        const knex = strapi.connections.default;
+        // const result1 = await knex('categories')
+        // .select('Name')
+        // console.log(result1)
 
-    //     const entity = await strapi.services.category.find();
-    //     // return sanitizeEntity(entity, { model: strapi.models.category });
-    //     return await strapi
-    //         .query('category')
-    //         .model.query(qb => {
-    //             qb.where('id', 1);
-    //         })
-    //         .fetch();
-    // },
+        const result = await knex('categories')
+            .select(knex.raw('??->? as name', ['Name', lang]), knex.raw('??->? as information', ['Information', lang]))
+        console.log(result)
+        return (_.groupBy(result, 'Name'));
+    },
 };
+
