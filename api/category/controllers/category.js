ca6671fda84c3072af437423779033351d172efb
DymaSv <dymasvyrydonov@gmail.com>
2021-04-19 22:59:11 -0400

    Added isActive field to show rows if value is true

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -4,77 +4,110 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-const { sanitizeEntity } = require('strapi-utils');
 const env = require('dotenv').config({ path: require('find-config')('.env') });
-const languages = env.parsed["LANGUAGES"]
-const defaultLanguage = env.parsed["DEFAULT_LANGUAGE"]
+const languages = env.parsed["LANGUAGES"];
+const defaultLanguage = env.parsed["DEFAULT_LANGUAGE"];
+const isActive = getActiveStatus();
+
+function getActiveStatus() {
+    let isActive = env.parsed["IS_ACTIVE"];
+    if (isActive === 'true') {
+        return true;
+    }
+    if (isActive === 'false') {
+        return false;
+    }
+    return null;
+}
+
+function getLanguage(ctx) {
+    if (!languages.includes(ctx.params.lang)) {
+        return defaultLanguage
+    }
+    return ctx.params.lang;
+}
+
 module.exports = {
 
     //Get all categories, not based on language
     //"path": "/categories"
     async findAll(ctx) {
-        let entities = await strapi.query('category').find();
-        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.category }));
+        const knex = strapi.connections.default;
+        const result = knex('categories')
+            .select(
+                "id",
+                "IdCategory as idCategory",
+                "IdParentCategory as idParentCategory",
+                'Name as name',
+                'Information as information'
+            )
+        if (isActive !== null) {
+            result.where('IsActive', isActive)
+        }
+        return await result;
     },
 
     //Get all categories, based on language
     //"path": "/:lang/categories"
     async find(ctx) {
-        let lang = ctx.params.lang;
-        if (!languages.includes(lang)) {
-            lang = defaultLanguage
-        }
+        let lang = getLanguage(ctx);
+        
         const knex = strapi.connections.default;
-        const result = await knex('categories')
+        const result = knex('categories')
             .select(
                 "id",
-                "IdCategory",
-                "IdParentCategory",
+                "IdCategory as idCategory",
+                "IdParentCategory as idParentCategory",
                 knex.raw('??->? as name', ['Name', lang]),
                 knex.raw('??->? as information', ['Information', lang])
             )
-        return result;
+        if (isActive !== null) {
+            result.where('IsActive', isActive)
+        }
+        return await result;
     },
 
     //Get main categories, based on language. Categories wich don't have IdParentCategory.
     //"path": "/:lang/categories/getMainCategories"
     async getMainCategories(ctx) {
-        let lang = ctx.params.lang;
-        if (!languages.includes(lang)) {
-            lang = defaultLanguage
-        }
+        let lang = getLanguage(ctx);
+        
         const knex = strapi.connections.default;
-        const entities = await knex('categories')
+        let result = knex('categories')
             .select(
                 "id",
-                "IdCategory",
-                "IdParentCategory",
+                "IdCategory as idCategory",
+                "IdParentCategory as idParentCategory",
                 knex.raw('??->? as name', ['Name', lang]),
                 knex.raw('??->? as information', ['Information', lang])
             ).where(function () {
                 this.where('IdParentCategory', null).orWhere('IdParentCategory', "")
             })
-        return entities;
+        if (isActive !== null) {
+            result.andWhere('IsActive', isActive)
+        }
+        return await result;
     },
 
     //Get categories, based on language and IdParentCategory. Nested categories wich have IdParentCategory.
     //"path": "/:lang/categories/getNestedCategories/:parentId"
     async getNestedCategories(ctx) {
-        let lang = ctx.params.lang;
         let parentId = ctx.params.parentId;
-        if (!languages.includes(lang)) {
-            lang = defaultLanguage
-        }
+        let lang = getLanguage(ctx);
+
         const knex = strapi.connections.default;
-        const entities = await knex('categories')
+        const result = knex('categories')
             .select(
                 "id",
-                "IdCategory",
-                "IdParentCategory",
+                "IdCategory as idCategory",
+                "IdParentCategory as idParentCategory",
                 knex.raw('??->? as name', ['Name', lang]),
                 knex.raw('??->? as information', ['Information', lang])
             ).where('IdParentCategory', parentId)
-        return entities;
+        if (isActive !== null) {
+            result.andWhere('IsActive', isActive)
+        }
+        return await result;
     },
 };
 
