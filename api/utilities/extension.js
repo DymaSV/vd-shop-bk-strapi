b2c499b6b795d745229c6e244a1136b14ef52ac8
DymaSv <dymasvyrydonov@gmail.com>
2021-05-13 13:36:34 -0400

    Added CORE and new Entities

--- api/utilities/extension.js
+++ api/utilities/extension.js
@@ -3,20 +3,23 @@ const env = require('dotenv').config({ path: require('find-config')('.env') });
 const languages = env.parsed["LANGUAGES"];
 const defaultLanguage = env.parsed["DEFAULT_LANGUAGE"];
 
-module.exports.getActiveStatus = function () {
-    let isActive = env.parsed["IS_ACTIVE"];
-    if (isActive === 'true') {
-        return true;
-    }
-    if (isActive === 'false') {
-        return false;
-    }
-    return null;
-}
 
-module.exports.getLanguage = function (ctx) {
-    if (!languages.includes(ctx.params.lang)) {
-        return defaultLanguage
+
+module.exports = {
+    getActiveStatus: () => {
+        let isActive = env.parsed["IS_ACTIVE"];
+        if (isActive === 'true') {
+            return true;
+        }
+        if (isActive === 'false') {
+            return false;
+        }
+        return null;
+    },
+    getLanguage: (ctx) => {
+        if (!languages.includes(ctx.params.lang)) {
+            return defaultLanguage
+        }
+        return ctx.params.lang;
     }
-    return ctx.params.lang;
 }
